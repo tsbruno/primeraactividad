daa74a17e06a94d5b574ccfceed054705c7acb70
bruno torres <tsoriabruno@gmail.com>
2021-12-15 22:19:26 -0600

    primeraactividadgeneration

---
+++ main.js
@@ -0,0 +1,31 @@
+
+
+let numero1 = 50; 
+let numero2 = 10.03; 
+let numero3 = -50; 
+let numero4 = "bruno"; 
+
+console.log(numero1, numero2, numero3, numero4);
+
+console.log("El valor del numero 1 es: " +numero1);
+console.log("El valor del numero 2 es: " +numero2);
+console.log("El valor del numero 3 es: " +numero3);
+console.log("El valor del numero 4 es: " +numero4);
+
+let miBoolean1 = true; 
+let miBoolean2 = false; 
+
+console.log("El valor de la variable 1 es: " + miBoolean1); 
+console.log("El valor de la variable 2 es: " + miBoolean2); 
+
+
+let variableVcia = null;
+console.log(variableVcia); 
+
+
+let vairable; 
+console.log(vairable); 
+
+
+variable = "palabra"; 
+console.log(variable); 
